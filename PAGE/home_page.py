fba6012e31e9f0916150edb3fa1db2bdf03c61c5
RED-ITA <enricosalvagiani@gmail.com>
2024-08-26 19:37:38 +0200

    collegate i richiami a funzioner dei pulsnati

--- PAGE/home_page.py
+++ PAGE/home_page.py
@@ -1,4 +1,3 @@
-
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QMainWindow, QApplication, QLabel
 from PyQt6.QtCore import QFile, QTextStream, QSize
 from PyQt6.QtGui import QColor, QPalette
@@ -30,27 +29,18 @@ class Home_Page(QWidget):
         self.h1 = QHBoxLayout()
         self.h2 = QHBoxLayout()
 
-        self.buttons = []
-        self.create_buttons()
         self.add_buttons_to_layout()
 
         self.setLayout(self.layout1)
 
-    def create_buttons(self):
-        # Create QPushButtonBadge instances
-        icons = ["warning.png", "settings.png", "eye.png", "home.png", "bell_orange.png", "bell_badge.png"]
-        for icon in icons:
-            button = q.QPushButtonBadge(icon)
-            self.buttons.append(button)
-
     def add_buttons_to_layout(self):
         size_ico = int(self.get_icon_size() / 2)
 
-        #navigation bar
+        # Navigation bar
         self.sos = q.QPushButtonBadge("sos.png")
         self.sos.setFixedSize(size_ico, size_ico)
 
-        #ora
+        # Ora
         v = QVBoxLayout()
         data = QLabel("Giorno N Mese")
         ora = QLabel("HH : MM")
@@ -58,13 +48,13 @@ class Home_Page(QWidget):
         v.addWidget(ora)
 
         self.layout_pagina = QHBoxLayout()
-        badge = q.QPushButtonBadge("home.png")
-        badge.setFixedSize(size_ico, size_ico)
+        self.home_button = q.QPushButtonBadge("home.png")
+        self.home_button.setFixedSize(size_ico, size_ico)
         label = QLabel("Home")
 
         self.layout_pagina.setSpacing(0)
         self.layout_pagina.addWidget(label)
-        self.layout_pagina.addWidget(badge)
+        self.layout_pagina.addWidget(self.home_button)
 
         self.prima_riga.addSpacing(self.get_icon_size())
         self.prima_riga.addWidget(self.sos)
@@ -73,17 +63,49 @@ class Home_Page(QWidget):
         self.prima_riga.addSpacing(self.get_icon_size())
 
         self.h1.addSpacing(int(self.master.width() * 0.2))
+
         # First row
-        for button in self.buttons[:3]:
-            button.setFixedSize(size_ico, size_ico)
-            self.h1.addWidget(button)
+        self.warning_button = q.QPushButtonBadge("warning.png")
+        self.warning_button.setFixedSize(size_ico, size_ico)
+        self.warning_button.clicked.connect(self.warning)
+        self.h1.addWidget(self.warning_button)
+
+        self.settings_button = q.QPushButtonBadge("settings.png")
+        self.settings_button.setFixedSize(size_ico, size_ico)
+        self.settings_button.clicked.connect(self.impostazioni)
+        self.h1.addWidget(self.settings_button)
+
+        self.sensors_button = q.QPushButtonBadge("eye.png")
+        self.sensors_button.setFixedSize(size_ico, size_ico)
+        self.sensors_button.clicked.connect(self.sensori)
+        self.h1.addWidget(self.sensors_button)
+
         self.h1.addSpacing(int(self.master.width() * 0.2))
 
         self.h2.addSpacing(int(self.master.width() * 0.2))
+
         # Second row
-        for button in self.buttons[3:]:
-            button.setFixedSize(size_ico, size_ico)
-            self.h2.addWidget(button)
+        self.rooms_button = q.QPushButtonBadge("home.png")
+        self.rooms_button.setFixedSize(size_ico, size_ico)
+        self.rooms_button.clicked.connect(self.stanze)
+        self.h2.addWidget(self.rooms_button)
+
+        self.activate_button = q.QPushButtonBadge("bell_orange.png")
+        self.activate_button.setFixedSize(size_ico, size_ico)
+        self.activate_button.clicked.connect(self.attiva)
+        self.h2.addWidget(self.activate_button)
+
+        self.deactivate_button = q.QPushButtonBadge("bell_slash_red.png")
+        self.deactivate_button.setFixedSize(size_ico, size_ico)
+        self.deactivate_button.clicked.connect(self.disattiva)
+        self.deactivate_button.setVisible(False)
+        self.h2.addWidget(self.deactivate_button)
+
+        self.detections_button = q.QPushButtonBadge("bell_badge.png")
+        self.detections_button.setFixedSize(size_ico, size_ico)
+        self.detections_button.clicked.connect(self.rilevamenti)
+        self.h2.addWidget(self.detections_button)
+
         self.h2.addSpacing(int(self.master.width() * 0.2))
 
         self.layout1.addLayout(self.prima_riga)
@@ -94,7 +116,6 @@ class Home_Page(QWidget):
         # Calculate icon size based on application window width
         return int(self.master.width() * 0.1)
 
-
     def resizeEvent(self, event):
         # Update icon size when application window size changes
         self.update_icon_size()
@@ -102,7 +123,9 @@ class Home_Page(QWidget):
 
     def update_icon_size(self):
         size_ico = self.get_icon_size()
-        for button in self.buttons:
+        for button in [self.warning_button, self.settings_button, self.sensors_button, 
+                       self.rooms_button, self.activate_button, self.deactivate_button, 
+                       self.detections_button, self.sos, self.home_button]:
             button.setFixedSize(size_ico, size_ico)
 
     def set_background_color(self):
@@ -119,3 +142,28 @@ class Home_Page(QWidget):
             style_sheet = stream.readAll()
             file.close()
             self.setStyleSheet(style_sheet)
+
+    def warning(self):
+        print("anomalie")
+        
+    def impostazioni(self):
+        print("impostazioni")
+        
+    def sensori(self):
+        print("sensori")
+
+    def stanze(self):
+        print("stanze")
+        
+    def attiva(self):
+        print("attiva")
+        self.activate_button.setVisible(False)
+        self.deactivate_button.setVisible(True)
+
+    def disattiva(self):
+        print("disattiva")
+        self.deactivate_button.setVisible(False)
+        self.activate_button.setVisible(True)
+        
+    def rilevamenti(self):
+        print("rilevamenti")
