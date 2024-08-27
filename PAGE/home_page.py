c593a65c4bb9c4e661cf8a7932e6aa8cb586e313
RED-ITA <enricosalvagiani@gmail.com>
2024-08-27 19:49:58 +0200

    data da guardare

--- PAGE/home_page.py
+++ PAGE/home_page.py
@@ -10,66 +10,37 @@ from CMP import QPushButtonBadge as q
 
 class Home_Page(QWidget):
 
-    def __init__(self, master):
+    def __init__(self, master, header):
         super().__init__()
 
         self.master = master
         self.master.setWindowTitle("HomePage")
         
+        
+        self.header = header
         self.initUI()
 
         self.setAutoFillBackground(True)
         self.set_background_color()
 
-        # Setup timer to update the time every second
-        self.setup_timer()
-
     def initUI(self):
         # Initialize the UI elements
         self.layout1 = QVBoxLayout()
-
-        self.prima_riga = QHBoxLayout()
+        
         self.h1 = QHBoxLayout()
         self.h2 = QHBoxLayout()
-
         self.add_buttons_to_layout()
 
         self.setLayout(self.layout1)
 
+        # Force update layout to ensure visibility
+        self.layout1.update()
+        self.layout1.activate()
+
+
     def add_buttons_to_layout(self):
         size_ico = int(self.get_icon_size() / 2)
 
-        # Navigation bar
-        self.sos = q.QPushButtonBadge("sos.png")
-        self.sos.setFixedSize(size_ico, size_ico)
-
-        # Ora
-        v = QVBoxLayout()
-        data = QLabel("Giorno N Mese")
-        data.setObjectName("data")
-        data.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
-        # Time label that will be updated
-        self.ora = QLabel("HH : MM")
-        self.ora.setObjectName("ora")
-        self.ora.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        v.addWidget(data)
-        v.addWidget(self.ora)
-
-        self.layout_pagina = QHBoxLayout()
-        self.home_button = q.QPushButtonBadge("home.png")
-        self.home_button.setFixedSize(size_ico, size_ico)
-
-        self.layout_pagina.setSpacing(0)
-        self.layout_pagina.addWidget(self.home_button)
-        
-        self.prima_riga.addSpacing(self.get_icon_size())
-        self.prima_riga.addWidget(self.sos)
-        self.prima_riga.addStretch()
-        self.prima_riga.addLayout(v)
-        self.prima_riga.addStretch()
-        self.prima_riga.addLayout(self.layout_pagina)
-        self.prima_riga.addSpacing(self.get_icon_size())
 
         self.h1.addSpacing(int(self.master.width() * 0.2))
 
@@ -117,24 +88,14 @@ class Home_Page(QWidget):
 
         self.h2.addSpacing(int(self.master.width() * 0.2))
 
-        self.layout1.addLayout(self.prima_riga)
+        
         self.layout1.addLayout(self.h1)
         self.layout1.addLayout(self.h2)
 
-    def setup_timer(self):
-        # Create a QTimer to update the time every second
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_time)
-        self.timer.start(1000)  # Update every 1000 milliseconds (1 second)
-
-    def update_time(self):
-        # Get the current time and set it to the QLabel
-        current_time = QTime.currentTime().toString("HH:mm")
-        self.ora.setText(current_time)
 
     def get_icon_size(self):
         # Calculate icon size based on application window width
-        return int(self.master.width() * 0.1)
+        return int((self.master.width() * 0.1) )
 
     def resizeEvent(self, event):
         # Update icon size when application window size changes
@@ -142,10 +103,11 @@ class Home_Page(QWidget):
         super().resizeEvent(event)
 
     def update_icon_size(self):
-        size_ico = self.get_icon_size()
+       
+        size_ico = int(self.get_icon_size())
         for button in [self.warning_button, self.settings_button, self.sensors_button, 
                        self.rooms_button, self.activate_button, self.deactivate_button, 
-                       self.detections_button, self.sos, self.home_button]:
+                       self.detections_button]:
             button.setFixedSize(size_ico, size_ico)
 
     def set_background_color(self):
@@ -168,6 +130,8 @@ class Home_Page(QWidget):
         
     def impostazioni(self):
         print("impostazioni")
+        self.header.set_tipo(1)
+        self.master.change_page(1)
         
     def sensori(self):
         print("sensori")
--- main_test.py
+++ main_test.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtGui
+from PyQt6.QtGui import  QColor
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QHBoxLayout, QStackedWidget
 
@@ -7,8 +7,8 @@ import sys
 
 from API import funzioni as f
 
-
-from PAGE import home_page as home
+from CMP import header as h 
+from PAGE import home_page as home, impostazioni_page as impo 
 
 class MainWindows(QMainWindow):
 
@@ -27,26 +27,44 @@ class MainWindows(QMainWindow):
         self.inizializzaUI()
         
     def create_layout(self):
-        self.main_layout = QStackedWidget()
+        wid = QWidget()
+        self.super_layout = QVBoxLayout()
+        self.super_layout.setContentsMargins(0,0,0,0)
         
-        #PAGINA 0
-        home_page = home.Home_Page(self)
+        self.main_layout = QStackedWidget()
+
+        self.header = h.Header(self)
+        self.super_layout.addWidget(self.header)
+        #PAGINA index 0
+        home_page = home.Home_Page(self, self.header)
         self.main_layout.addWidget(home_page)
         
-        #PAGINA 1
+        #PAGINA index 1
+        impo_page = impo.Impostazioni_Page(self, self.header)
+        self.main_layout.addWidget(impo_page)
         
-        
-        self.setCentralWidget(self.main_layout)
-    
+        self.super_layout.addWidget(self.main_layout)
 
+        wid.setLayout(self.super_layout)
+        self.setCentralWidget(wid)
+
+        wid.setAutoFillBackground(True)
+        self.set_background_color()
+    
+    def set_background_color(self):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor.fromRgb(241, 241, 241))
+        self.setPalette(p)
         
     def inizializzaUI(self):
-
-        
         self.main_layout.setCurrentIndex(0) # parte dall widget 0 
+        self.header.set_tipo(0)
 
-        
-
+    def change_page(self, index):
+        self.main_layout.setCurrentIndex(index)
+        if index == 0:
+            self.header.set_tipo(0)
+            
 
 
 if __name__ == "__main__":
